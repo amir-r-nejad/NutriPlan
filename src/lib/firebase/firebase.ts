07888600a49dedcf8748161f57927730a1d96cdf
Mirzaei81 <aam.miezaei@gmail.com>
2025-06-09 21:50:16 +0330

    using prive .env file

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -3,10 +3,13 @@ import admin, { ServiceAccount } from 'firebase-admin';
 import {firebaseConfig} from "../constants"
 
 
-
-console.log("Server config",firebaseConfig)
+let serviceAccount:ServiceAccount = {
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || firebaseConfig.projectId,
+        clientEmail: process.env.CLIENT_EMAIL,
+        privateKey: process.env.PRIVATE_KEY
+}
 // Initialize Firebase
-const app = admin.apps.length == 0 ? admin.initializeApp(firebaseConfig) : admin.app();
+const app = admin.apps.length == 0 ? admin.initializeApp({ credential: admin.credential.cert(serviceAccount) }) : admin.app();
 const auth = admin.auth(app);
 const db = admin.firestore(app);
 const storage = admin.storage(app);
