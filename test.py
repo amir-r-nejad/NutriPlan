54c890736e165e00f0575d880cbe93957f815e13
Mirzaei81 <aam.miezaei@gmail.com>
2025-06-09 14:49:18 +0330

    work on local
    	modified:   .gitignore
    	modified:   src/ai/flows/adjust-meal-ingredients.ts
    	modified:   tsconfig.json

--- test.py
+++ test.py
@@ -1,39 +1,39 @@
-n = 4
-def isValid(board) :
-	for i in range(n) :
-		for j in range(n):
-			if board[i][j]=="Q":
-				for k in range(j+1,n):
-					if i+k-j>=n:
-						break
-					if board[i+k-j][k]=="Q":
-						for w in range(n,i,-1):
-							board.pop()
-						return False
-				for k in range(j-1,0,-1):
-					if i-k+j>=n:
-						break
-					if board[i-k+j][k]=="Q":
-						for w in range(n,i,-1):
-							board.pop()
-						return False
-				break
-	
-	return True
-b1 = [".Q..",
- ".Q..",
- "...Q",
- "Q..."]
+from google.cloud import secretmanager
+from google.oauth2 import service_account
+from google.api_core import exceptions
+import grpc
 
-b2 =["..Q.",
- "Q...",
- "...Q",
- ".Q.."]
-b4 = [".Q..",
- "...Q",
- "Q...",
- "..Q."]
+PROJECT_ID = "nutriplan-7wkxu"
+credentials = service_account.Credentials.from_service_account_file('service_acc_key.json')
+client = secretmanager.SecretManagerServiceClient(credentials=credentials)
 
-print(isValid(b1))
-print(isValid(b2))
-print(isValid(b4))
+
+def create_secret(secret_id,payload):
+    # Create the Secret Manager client.
+	client = secretmanager.SecretManagerServiceClient()
+    # Build the resource name of the parent project.
+	parent = f"projects/{PROJECT_ID}"
+    # Build a dict of settings for the secret
+	secret = {'replication': {'automatic': {}}}
+    # Create the secret
+	try:
+		response = client.create_secret(secret_id=secret_id, parent=parent, secret=secret)
+		print(f'Added secret : {response.name}')   
+	except exceptions.AlreadyExists as e:
+		print(f'Secret {secret_id} already exists, skipping creation')
+		return
+	parent = f"projects/{PROJECT_ID}/secrets/{secret_id}"
+
+    # Convert the string payload into a bytes. This step can be omitted if you
+    # pass in bytes instead of a str for the payload argument.
+	payload = payload.encode('UTF-8')
+
+    # Add the secret version.	
+	response = client.add_secret_version(parent=parent, payload={'data': payload})
+
+    # Print the new secret version name.
+	print(f'Added secret version: {response.name}')   
+
+f = open(".env")
+while line := f.readline():
+	print(f"firebase apphosting:secrets:grantaccess {line.split('=')[0]} -b {PROJECT_ID} --project {PROJECT_ID}")
